977f5c2425994414f6420a4b6af1db70584bf56b
mr-riad <riadgangni01302@gmail.com>
2025-06-25 00:30:01 +0600

    product dialog added

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -8,10 +8,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void productDialog() {
+    TextEditingController productNameController = TextEditingController();
+    TextEditingController productQtyController = TextEditingController();
+    TextEditingController productImgController = TextEditingController();
+    TextEditingController ProductUnitPriceController = TextEditingController();
+    TextEditingController ProductTotalPriceController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text('Add product'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter the product name',
+                  ),
+                ),
+              ],
+            ),
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Product CURD",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
 
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => productDialog(),
+
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
